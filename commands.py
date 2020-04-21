e5da9eb3803db2bbdfae8c1dbfe177a047f95774
mcnutty <william.seymour@kellogg.ox.ac.uk>
2020-04-21 15:49:47 +0100

    replaced rand with roll, added flip

--- commands.py
+++ commands.py
@@ -5,16 +5,44 @@ import random
 def say(db_session, *args):
     return [' '.join(args)]
 
-def rand(db_session, *args):
-    if len(args) != 2:
-        return ['!rand <min> <max>']
-    try:
-        return [str(random.randint(int(args[0]), int(args[1])))]
-    except ValueError:
-        return ['Please give me integers']
+def flip(db_session, *args):
+    if len(args) == 0:
+        return ["🖕"]
+    result = random.randint(0, len(args)-1)
+    return [args[result]]
 
-def randf(db_session, *args):
-    return [str(random.random())]
+def roll(db_session, *args):
+    if len(args) != 1 and len(args) != 2:
+        return ['!roll <x>d<y> <opt> - roll a y sided die x times (optionally take the high or sum)']
+    pos = args[0].find('d')
+    if pos == -1:
+        return ['!roll <x>d<y> <opt> - roll a y sided die x times (optionally take the high or sum)']
+        
+    if random.randint(0, 100) == 0:
+        return ['NUMBERWANG']
+
+    s = 0
+    h = 0
+    result = ''
+
+    try:
+        for dice in range(int(args[0][:pos])):
+            if result != '':
+                result += ', '
+            outcome = random.randint(1, int(args[0][pos+1:]))
+            if h < outcome:
+                h = outcome
+            s += outcome
+            result += str(outcome)
+    except ValueError:
+        return ["Please give me integers!"]
+
+    if len(args) == 2 and args[1] == 'high':
+        result += f" (high={h})"
+    elif len(args) == 2 and args[1] == 'sum':
+        result += f" (sum={s})"
+
+    return [result]
 
 def karma(db_session, *args):
     reply = ''
