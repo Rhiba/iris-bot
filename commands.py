1a1825aa16e03c8e813313d9b6f697414fc87056
Capputch <esther.rzewski@gmail.com>
2021-01-24 19:45:47 +0000

    Add poems database and poems commands

--- commands.py
+++ commands.py
@@ -4,8 +4,8 @@ import random
 import dateutil
 import re
 from utils.quotes import quotes
-from data import poems
-
+import yaml
+    
 def gym_notify(db_session, message, *args):
     if not len(args) == 2: 
         print('Wrong number of arguments.')
@@ -179,8 +179,47 @@ def uplift(db_session, message, *args):
     return [x]
 
 def poem(db_session, message, *args):
-   for author in poems:
-        if author == args:
-           return ['yup!']
+    with open('data/poems.yaml') as f:
+        data = yaml.load(f, Loader=yaml.FullLoader) 
+    #if no argument given, post a random poem
+    if len(args) == 0: 
+        poems_list = list(data.items())
+        poet, poems = random.choice(poems_list)
+        poem_name, poem = random.choice(list(poems.items()))
+        return [poem_name + " by " + poet.title() + "\n\n" + poem]
+    #if an author is specified, post a random poem by that poet
+    author = " ".join(args).lower()
+    if author in data:  
+        poems = data[author] 
+        poem_name, poem = random.choice(list(poems.items()))
+        return [poem_name + "\n\n" + poem]
+    if author not in data:
+        return ["Sorry, I don't have any poems by that author. Why not add a poem using !addpoem?"]
 
+def printpoems(db_session, message, *args):
+    with open('data/poems.yaml') as f:
+        data = yaml.load(f, Loader=yaml.FullLoader) 
+    #Post all poems by the specified author
+    author = " ".join(args).lower()
+    for author in data: 
+        poems = data[author]
+        poems_list = list(poems)
+        return ["Poems by " + author + " include: \n" + str(poems_list)]
+
+def addpoem(db_session, message, *args):
+    with open('data/poems.yaml') as f:
+        data = yaml.load(f, Loader=yaml.FullLoader) 
+    author, title, poem = " ".join(args).split(',', maxsplit=2)
+    poem = poem.strip()
+    #if author is already there
+    if author in data:
+        data[author][title] = poem
+    #if author is not already there, create a new entry 
+    else:
+        data[author] = {title : poem}
+    with open('data/poems.yaml', 'w') as f:
+        yaml.dump(data, f)
+    return ['I have added your poem to my database']
     
+
+
